94554b8d0f8846d44b73aa3711a91120fe10cc92
HagerKhorboz <hager.khorboz@gmail.com>
2023-04-27 14:59:15 -0700

    remove else from is digit c file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /*
  * _isdigit - Checks if a character is digit from 0 to 9
- * @t: The character to be checked
+ * @t: The number to be checked
  *
  * Return: 1 if the character is digit, 0 otherwise
  */
@@ -13,9 +13,6 @@ int _isdigit(int x)
 	{
 		return (1);
 	}
-	else
-	{
 		return (0);
-	}
 }
 
