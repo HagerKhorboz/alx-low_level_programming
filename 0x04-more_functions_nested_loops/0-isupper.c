cc359bdd46fc4235b6c277898262c020111751f0
HagerKhorboz <hager.khorboz@gmail.com>
2023-04-27 14:29:02 -0700

    change the comment to match betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,21 @@
 #include "main.h"
-/* Check if the letter is upper or not */
+
+/**
+ * _isupper - Checks if a character is uppercase
+ * @t: The character to be checked
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise
+ */
 
 int _isupper(int t)
 {
 	if (t >= 65 && t <= 90)
+	{
 		return 1;
+	}
 	else
+	{
 		return 0;
+	}
 }
 
